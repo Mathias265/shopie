5e4e89ed2d7535e7e16645e745c73e78e7c878e4
Mathias265 <mathiasokafor47@gmail.com>
2024-06-18 00:50:05 +0100

    Update navigate.vue

--- components/home/navigate.vue
+++ components/home/navigate.vue
@@ -25,8 +25,12 @@
       <transition name="slide-fade">
         <div v-if="isOpen" class="md:hidden z-10 absolute top-20 left-0 right-0">
        
-        <ul class="bg-white">
-          
+        <ul class="bg-white ">
+          <li><NuxtLink to="/" class="text-black hover:text-orange-300 block py-2 px-4 hover:bg-blue-500 hover:text-white">Home</NuxtLink></li>
+            <li><NuxtLink to="/" class="text-black hover:text-orange-300 block py-2 px-4 hover:bg-blue-500 hover:text-white">Services</NuxtLink></li>
+            <li><NuxtLink to="/about" class="text-black hover:text-orange-300 block py-2 px-4 hover:bg-blue-500 hover:text-white">About Us</NuxtLink></li>
+            <li><NuxtLink to="/" class="text-black hover:text-orange-300 block py-2 px-4 hover:bg-blue-500 hover:text-white">Blog Post</NuxtLink></li>
+            <li><NuxtLink to="/" class="text-black hover:text-orange-300 block py-2 px-4 hover:bg-blue-500 hover:text-white">Contacts</NuxtLink></li>
             
           <button class="hidden md:block btn-3d bg-gradient-to-r from-blue-400 to-blue-600 text-white font-bold py-1 px-6 rounded-lg shadow-lg hover:shadow-2xl focus:outline-none" @click="goBack()">log-out</button>
             
